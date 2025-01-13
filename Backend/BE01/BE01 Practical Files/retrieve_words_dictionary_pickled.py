e7388908a807fbb7473ecc6e64227a737ae55199
Nathan Baxter <baxter-n3@ulster.ac.uk>
2025-01-13 15:47:29 +0000

    15.47

--- Backend/BE01/BE01 Practical Files/retrieve_words_dictionary_pickled.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-fin = open("words_dictionary.txt", "rb")
-words_dict = pickle.load(fin)
-fin.close()
-
-letters = "abcdefghijklmnopqrstuvwxyz"
-for letter in letters:
-    print(letter, words_dict[letter], '\n')
--- Frontend/FE12/FE12/src/app/data.service.ts
+++
@@ -1,6 +0,0 @@
-import jsonData from '../assets/businesses.json'
-export class DataService {
-getBusinesses() {
-return jsonData;
-}
-}
